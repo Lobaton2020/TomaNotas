1714daaa9500739d847c422d92f4b5f3283863b9
Andres Felipe <andrespipe021028@gmail.com>
2020-09-05 14:12:41 -0500

    Prepare the frameworks for begin to make the app

--- views/template/auth/content.php
+++
@@ -1,9 +0,0 @@
-<?php
-      require_once "views/template/auth/header.php";
-?>
-
-      <?php  require_once "views/$content"?>
-      
-
-
-<?php  require_once "views/template/auth/footer.php"; ?>
--- views/template/auth/footer.php
+++
@@ -1,9 +0,0 @@
-
-   <script src="libs/fontawesome/js/all.min.js"></script>
-   <script src="libs/jquery/jquery.min.js"></script>
-   <script src="libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-   <script src="libs/bootstrap/js/bootstrap.min.js"></script>
-   
-   <script src="assets/js/Auth.js"></script>
-  </body>
-</html> 
