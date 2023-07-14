c6c7824a0ea1c49289d6db1a3d8a07ba322fd9a8
Andres Lobaton <andrespipe021028@gmail.com>
2023-07-13 23:55:45 -0500

    fix: error print message

--- assets/js/Cronograma.js
+++ assets/js/Cronograma.js
@@ -97,11 +97,117 @@ function setWeekNumber() {
 
 }
 
-const initialize = () => {
+function requestHttp(method, url) {
+    return new Promise(resolve => {
+
+        let http = new XMLHttpRequest();
+        http.addEventListener("load", async (event) => {
+            return resolve(JSON.parse(event.currentTarget.responseText));
+        });
+        http.open(method, url)
+        http.send(null);
+    })
+}
+
+function renderTimeSelect() {
+    let element = document.querySelector("#hora");
+    for (let i = 0; i <= 24; i++) {
+        let number = i,
+            option = document.createElement("option");
+        option.value = number;
+        option.textContent = number;
+        if (i === 12) {
+            option.selected = "on";
+
+        }
+        element.appendChild(option);
+    }
+}
+function textToSpeech(text) {
+    return new Promise((resolve) => {
+        const voices = window.speechSynthesis.getVoices();
+        const voice = voices.findIndex(voice => voice.lang === 'en-HK');
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.voice = voices[voice + 1];
+        utterance.addEventListener('end', () => resolve(true))
+        window.speechSynthesis.speak(utterance);
+    })
+
+}
+function renderNotificationSound(title, description) {
+    let notification;
+    let options = {
+        "body": description,
+        "icon": "assets/img/logo.png"
+    };
+    if (Notification.permission === "granted") {
+        notification = new Notification(title, options)
+        window.audio.play();
+        notification.addEventListener("click", closeNotification);
+        notification.addEventListener("close", closeNotification);
+        setTimeout(closeNotification, 60 * 1000)
+    } else {
+        Notification.requestPermission((permiso) => {
+            if (permiso === "granted") {
+                notification = new Notification(title, options);
+                window.audio.play();
+                notification.addEventListener("click", closeNotification);
+                notification.addEventListener("close", closeNotification);
+                setTimeout(closeNotification, 60 * 1000)
+            }
+        });
+    }
+}
+
+function closeNotification(event) {
+    event?.currentTarget?.close()
+    audio.pause();
+    elemTaskId.classList.remove("active-action")
+
+}
+
+function activeAlarms(data, title, username) {
+    let currentDate = new Date();
+    let dateTask = new Date();
+    data.forEach(async (elem) => {
+        let {
+            id_tarea_cronograma_PK,
+            descripcion,
+            hora,
+            minuto
+        } = elem;
+        dateTask.setHours(hora, minuto, 0);
+        if (localStorage.getItem("isDebug")) {
+            console.log(dateTask.getTime() == currentDate.getTime(), dateTask.getTime(), currentDate.getTime())
+        }
+        if (dateTask.getTime() == currentDate.getTime()) {
+            await textToSpeech(`
+            Hi ${username}.
+            You have a task: ${descripcion}`)
+            renderNotificationSound(descripcion, "Tienes un deber!");
+            window.elemTaskId = document.querySelector(`#task-id-${id_tarea_cronograma_PK}`);
+            elemTaskId.classList.add("active-action")
+        }
+    });
+}
+
+const initialize = async () => {
+    window.audio = document.createElement("audio")
+    window.audio.src = "assets/audio/rington-mario-bros.mp3";
+    window.audio.volume = 0.4;
+    window.audio.loop = true
     setWeekNumber();
+    renderTimeSelect();
+    textToSpeech("");
+
     $.get('?c=project&format=json&status=1', (data) => {
         const string = data.reduce((acc, act) => `<option ${act.name.toLowerCase().includes("default") ? "selected":""} value="${act.id}">${act.name}</option>` + acc, '')
         $("#project_id").html(`<option value="">None</option>${string}`)
     })
+
+    const url = new URLSearchParams(location.search)
+    let { username, data, titulo } = await requestHttp("GET", `?c=cronograma&m=getTareasJSON&id=${url.get("id")}`)
+    setInterval(() => activeAlarms(data, titulo.titulo, username), 1000);
+
 }
 document.addEventListener("DOMContentLoaded", initialize)
